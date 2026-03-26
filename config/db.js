213e97df9710c319ca1b18fdee08fb9e423c1ce1
developer-kpit <ibrahim1530388@gmail.com>
2026-03-26 16:39:13 +0500

    fix: switch to postgres

--- config/db.js
+++ config/db.js
@@ -46,3 +46,17 @@ export default db.promise();
 //   .catch((err) => console.error("❌ DB connection error:", err));
 
 // export default pool;
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool
+  .connect()
+  .then(() => console.log("✅ Database connected"))
+  .catch((err) => console.error("❌ DB error:", err));
