118389f9dba0ace0cb6214a4e7b3798579aec57a
developer-kpit <ibrahim1530388@gmail.com>
2026-03-27 11:08:07 +0500

    Fix merge conflicts in appointmentRoutes.js

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -3,25 +3,22 @@ import {
   fetchAppointments,
   addAppointment,
   removeAppointment,
-<<<<<<< HEAD
   editAppointment,
 } from "../controllers/appointmentController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// Get all appointments (protected)
 router.get("/", verifyToken, fetchAppointments);
+
+// Add new appointment (protected)
 router.post("/", verifyToken, addAppointment);
+
+// Edit appointment (protected)
 router.put("/:id", verifyToken, editAppointment);
+
+// Delete appointment (protected)
 router.delete("/:id", verifyToken, removeAppointment);
-=======
-} from "../controllers/appointmentController.js";
-
-const router = express.Router();
-
-router.get("/", fetchAppointments);
-router.post("/", addAppointment);
-router.delete("/:id", removeAppointment);
->>>>>>> 931e1ee7d492884b6a6ae522f21133eab016e868
 
 export default router;
